e167647df242e1231dc38e03e4dcf13d50ef5e98
NadaRamadan <nada7.ra@gmail.com>
2024-11-02 14:29:57 +0200

    added listview to the story

--- lib/main.dart
+++ lib/main.dart
@@ -30,68 +30,101 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor:Colors.white,
-        elevation:0.0,
-        title: Row(
-          children: [
-            CircleAvatar(
-              radius: 20.0,
-              backgroundImage: AssetImage("Images/logo.jpeg"),
-            ),
-            SizedBox(width: 20),
-            Text(
-              "chats",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
-        actions: [
-          IconButton(
-            onPressed: (){},
-            icon: CircleAvatar(
-              backgroundColor: Colors.blue,
-              child: Icon(Icons.camera_alt_outlined,color: Colors.white,),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0.0,
+          title: Row(
+            children: [
+              CircleAvatar(
+                radius: 20.0,
+                backgroundImage: AssetImage("Images/logo.jpeg"),
+              ),
+              SizedBox(width: 20),
+              Text(
+                "chats",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
           ),
-          IconButton(
-            onPressed: (){},
-            icon: CircleAvatar(
-              backgroundColor: Colors.blue,
-              child: Icon(Icons.edit, color: Colors.white,),
-            ),
-          )
-        ],
-      ),
-      body:Container(
-  
-        padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
-        child: Column(
-          
-          children: [
-            Container(
-            width: 600,
-            height: 50,
-            decoration: BoxDecoration(
-                        color:Colors.grey[300],
-borderRadius: BorderRadius.circular(5.0)
-
-            ),
-              child: TextField(
-              
-               decoration: InputDecoration(
-                border: InputBorder.none,
-                icon: Icon(Icons.search),
-               hintText: "search",
-
-               ),
-
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.camera_alt_outlined,
+                  color: Colors.white,
+                ),
               ),
             ),
-            
+            IconButton(
+              onPressed: () {},
+              icon: CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.edit,
+                  color: Colors.white,
+                ),
+              ),
+            )
           ],
         ),
-      )
-    );
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+          child: Column(
+            children: [
+              Container(
+                width: 600,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(5.0)),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    icon: Icon(Icons.search),
+                    hintText: "search",
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.0,),
+              Container(
+                height: 110,
+                child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 7,
+                    itemBuilder: (context, i) => buildStoryItem(),
+                    separatorBuilder: (context, i) => SizedBox(
+                          width: 10,
+                        )),
+              )
+            ],
+          ),
+        ));
   }
 }
+
+Widget buildStoryItem() => Container(
+      width: 70.0,
+      child: Column(
+        children: [
+          Stack(
+            alignment:AlignmentDirectional.bottomEnd,
+            children: [
+              CircleAvatar(
+                radius: 30,
+                backgroundImage: AssetImage('Images/logo.jpeg'),
+              ),
+              Padding(
+                padding:  EdgeInsetsDirectional.only(bottom: 3.0, end:3.0),
+                child: CircleAvatar(
+                  radius: 5.0,
+                  backgroundColor: Colors.green,
+                ),
+              )
+            ],
+          ),
+          Text("Nada Ramadan", textAlign: TextAlign.center,)
+        ],
+      ),
+    );
